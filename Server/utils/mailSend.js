bcf2e6c05352afa65e12caf937b22275ab32e9ac
Manjeet <146162328+Digigloobal@users.noreply.github.com>
2026-08-18 16:35:18 +0530

    Add family option to mail server configuration

--- Server/utils/mailSend.js
+++ Server/utils/mailSend.js
@@ -11,6 +11,7 @@ const mailSend = async (email,title,body) => {
            host: "smtp.gmail.com",
             port: 587,
             secure: false,
+             family: 4,
             auth:{
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
